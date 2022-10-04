25d1374982c671b87850ba448fecfedf9762fc01
ChernV <vchern185@gmail.com>
2022-10-04 22:54:18 +0300

    DataFrameColumn_func.cs added

--- sem-2022-10-04/DataFrameColumn_func.cs
+++ sem-2022-10-04/DataFrameColumn_func.cs
@@ -1,6 +1,6 @@
 // Solution: hse - sem-2022-10-04 - DataFrameColumn_func.cs
 // Created at 2022-10-04 22:51
-// Author: Черных Владимир Артемович
+// Author: Рудинский Матвей Александрович 
 // Group: БПИ229
 
 namespace sem_2022_10_04;
