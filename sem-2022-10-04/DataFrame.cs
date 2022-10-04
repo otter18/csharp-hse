c67cf46ad15db8e3bb419eff94b811fd24e8ac92
ChernV <vchern185@gmail.com>
2022-10-04 20:39:27 +0300

    DataFrame.cs DataFrameColumns added

--- sem-2022-10-04/DataFrame.cs
+++ sem-2022-10-04/DataFrame.cs
@@ -7,15 +7,18 @@ namespace sem_2022_10_04;
 
 internal partial class DataFrame
 {
-    private Dictionary<string, List<object>> _data;
+    private Dictionary<string, DataFrameColumn> _data = { };
 
     public DataFrame(Dictionary<string, List<object>> data)
     {
-        _data = data;
+        foreach (var key in data.Keys)
+        {
+            _data.Add(key, new DataFrameColumn(key, data[key]));
+        }
     }
 
     public string[] Columns => _data.Keys.ToArray();
-    public Tuple<int, int> Shape => new Tuple<int, int>(_data.Keys.Count, _data[this.Columns[0]].Count);
+    public Tuple<int, int> Shape => new Tuple<int, int>(_data.Keys.Count, _data[this.Columns[0]].Len);
 
     public override string ToString()
     {
