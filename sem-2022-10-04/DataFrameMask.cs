de7b6d5d7b1582335b6768ff25db0ef282fc561b
ChernV <vchern185@gmail.com>
2022-10-04 19:56:51 +0300

    DataFrameMask.cs added

---
+++ sem-2022-10-04/DataFrameMask.cs
@@ -0,0 +1,50 @@
+// Solution: hse - sem-2022-10-04 - DataFrameMask.cs
+// Created at 2022-10-04 19:44
+// Author: Черных Владимир Артемович
+// Group: БПИ229
+
+using System.Text.Json;
+
+namespace sem_2022_10_04;
+
+public class DataFrameMask
+{
+    public int len => _mask.Count;
+    public List<bool> _mask;
+
+    public bool this[int index]
+    {
+        get => _mask[index];
+        set => _mask[index] = value;
+    }
+
+    public static DataFrameMask operator &(DataFrameMask mask1, DataFrameMask mask2)
+    {
+        var resMask = new DataFrameMask(mask1.len);
+        for (int i = 0; i < resMask.len; i++)
+        {
+            resMask[i] = mask1[i] & mask2[i];
+        }
+
+        return resMask;
+    }
+
+    public DataFrameMask(int len)
+    {
+        _mask = new List<bool>(len);
+        for (var i = 0; i < len; i++)
+        {
+            _mask.Add(false);
+        }
+    }
+
+    public DataFrameMask(List<bool> mask)
+    {
+        _mask = mask;
+    }
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(_mask);
+    }
+}
--- sem-2022-10-04/Program.cs
+++ sem-2022-10-04/Program.cs
@@ -1,13 +1,31 @@
 ﻿using sem_2022_10_04;
 
-var a = new DataFrame(new Dictionary<string, List<object>>
-{
-    {
-        "A", new List<object> { 1, 2, 3 }
-    },
-    {
-        "B", new List<object> { 4, 5, 6 }
-    }
-});
+// var a = new DataFrame(new Dictionary<string, List<object>>
+// {
+//     {
+//         "A", new List<object> { 1, 2, 3 }
+//     },
+//     {
+//         "B", new List<object> { 4, 5, 6 }
+//     }
+// });
+
+var mask1 = new DataFrameMask(new List<bool> { false, true, true });
+var mask2 = new DataFrameMask(new List<bool> { false, true, false });
+var mask3 = mask1 & mask2;
+
+Console.WriteLine(mask3);
+
+/*
+A B
+1 4
+2 5
+3 6    
 
 
+a[f() => a["A"]]
+
+a[a["A"] == 10]
+a[(a["A"] == 10) & (a["B"] > 10)]
+
+*/
