b087bb45587a67298c1b404b7cd787fbcd831908
userviktoriuss <tf349@mail.ru>
2022-10-04 20:44:00 +0300

    added new operations for DataFrameColumn.cs, deleted DataFrameVitya.cs

--- sem-2022-10-04/DataFrameColumn.cs
+++ sem-2022-10-04/DataFrameColumn.cs
@@ -1,4 +1,9 @@
-﻿namespace sem_2022_10_04;
+﻿// Solution: hse - sem-2022-10-04 - DataFrame.cs
+// Created at 2022-10-04 19:01
+// Author: Филимонов Виктор Павлович
+// Group: БПИ229
+
+namespace sem_2022_10_04;
 
 public class DataFrameColumn
 {
@@ -26,6 +31,34 @@ public class DataFrameColumn
     public static DataFrameMask operator !=(DataFrameColumn a, object b)
     {
         return !(a == b);
+    }
+
+    public static DataFrameMask operator >(DataFrameColumn a, object b)
+    {
+        var resMask = new DataFrameMask(a.Len);
+        
+        if (!double.TryParse(b.ToString(), out var doubleB)) return resMask;
+        
+        for (var i = 0; i < a.Len; ++i)
+        {
+            resMask[i] = double.Parse(a[i].ToString()!) > doubleB;
+        }
+        return resMask;
+    }
+
+    public static DataFrameMask operator <(DataFrameColumn a, object b)
+    {
+        return (a != b) & !(a > b); // а это правда вообще?
+    }
+
+    public static DataFrameMask operator >=(DataFrameColumn a, object b)
+    {
+        return (a == b) | (a > b);
+    }
+
+    public static DataFrameMask operator <=(DataFrameColumn a, object b)
+    {
+        return (a == b) | (a < b);
     }
 
     public DataFrameColumn(string name, List<object> data)
