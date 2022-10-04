66124259e166e20082ecf1bc1d7b037ba54c677c
userviktoriuss <tf349@mail.ru>
2022-10-04 20:13:58 +0300

    DataFrameColumn.cs added & cleared my Vitya's file

---
+++ sem-2022-10-04/DataFrameColumn.cs
@@ -0,0 +1,36 @@
+﻿namespace sem_2022_10_04;
+
+public class DataFrameColumn
+{
+    public string Name { get; set; };
+    private List<object> _data;
+    public int Len => _data.Count;
+    
+    public object this[int index]
+    {
+        get => _data[index];
+        set => _data[index] = value;
+    }
+    
+    public static DataFrameMask operator ==(DataFrameColumn a, object b)
+    {
+        var resMask = new DataFrameMask(a.Len);
+        for (int i = 0; i < a.Len; ++i)
+        {
+            resMask[i] = a[i] == b;
+        }
+
+        return resMask;
+    }
+
+    public static DataFrameMask operator !=(DataFrameColumn a, object b)
+    {
+        return !(a == b);
+    }
+
+    public DataFrameColumn(string name, List<object> data)
+    {
+        Name = name;
+        _data = data;
+    }
+}
--- sem-2022-10-04/DataFrameVitya.cs
+++ sem-2022-10-04/DataFrameVitya.cs
@@ -1,6 +1,14 @@
-﻿namespace sem_2022_10_04;
+﻿// Solution: hse - sem-2022-10-04 - DataFrame.cs
+// Created at 2022-10-04 19:01
+// Author: Филимонов Виктор Павлович
+// Group: БПИ229
 
-public class DataFrameVitya
+namespace sem_2022_10_04;
+
+public partial class DataFrame
 {
+    // GOAL:
+    // a['name'] == 3
+    
     
 }
