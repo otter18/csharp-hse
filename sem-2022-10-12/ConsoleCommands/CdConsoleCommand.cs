0bce75ac50757dc3d1c13e55bdc000c882c9251c
totandras <achtot@edu.hse.ru>
2022-10-16 15:09:11 +0300

    #1 Implemented CdConsoleCommand class.

--- sem-2022-10-12/ConsoleCommands/CdConsoleCommand.cs
+++ sem-2022-10-12/ConsoleCommands/CdConsoleCommand.cs
@@ -1,26 +1,165 @@
 // Solution: hse - sem-2022-10-12 - CdConsoleCommand.cs
 // Created at 2022-10-15 22:23
-// Author: 
+// Author: Тот Андраш
 // Group: БПИ229
 
 namespace sem_2022_10_12;
 
+using System.IO;
+
+/// <summary>
+/// Represents Linux-alike cd command of console.
+/// </summary>
 public class CdConsoleCommand : IConsoleCommand
 {
-    // BUG: Current implementation is for testing. Improve it!
-    // TODO: Changes currentDir accordingly to passed params. Should support ., / and other special paths. Can't go outside sandbox/
-    public ConsoleState Process(string inpCommand, DirectoryInfo currentDir)
+    /// <summary>
+    /// Reorganizing the path so it is indifferent to different operating systems.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns>Indifferent to OS path.</returns>
+    private string MakePathIndifferent(string path)
     {
-        var newCurrentDir = new DirectoryInfo(currentDir + "/" + inpCommand.Split(" ")[1]);
-        return new ConsoleState
+        // Path.DirectorySeparatorChar is vital! DO NOT REMOVE! 
+        return Path.DirectorySeparatorChar +
+               Path.Combine(path.Split('/', '\\'));
+    }
+
+    /// <summary>
+    /// Checks if the root directory of the ConsoleEngine is parent
+    /// of the given directory. 
+    /// </summary>
+    private bool IsInRoot(DirectoryInfo directoryToCheck)
+    {
+        return directoryToCheck.FullName.StartsWith(ConsoleEngine.RootDir.FullName);
+    }
+
+    /// <summary>
+    /// Returns ConsoleState object with the same directory as it was
+    /// before calling cd command with the description of the reason why
+    /// the directory hasn't changed.
+    /// </summary>
+    private ConsoleState NoMoveDueError(
+        string errorDescription,
+        DirectoryInfo currentDirectory)
+    {
+        return new ConsoleState()
         {
-            CurrentDir = newCurrentDir,
-            Result = ""
+            CurrentDir = currentDirectory,
+            Result = errorDescription
         };
     }
 
-    public string GetHelpMessage()
+    /// <summary>
+    /// Gets the path the user requests in a ready to use form.
+    /// </summary>
+    private string GetRequestedPath(string path)
     {
-        return "cd {path}";
+        var pathSplitted = path.Split(" ",
+            StringSplitOptions.RemoveEmptyEntries);
+
+        if (pathSplitted.Length < 2)
+        {
+            throw new CdCommnandEmptyPathException();
+        }
+        
+        return MakePathIndifferent(pathSplitted[1]);
     }
+
+    /// <summary>
+    /// Returns a directory is supposed to be requested by the user.
+    /// </summary>
+    private DirectoryInfo ManagePath(string requestedPath, string currentPath)
+    {
+        // If the command was cd . than return the current directory.
+        if (requestedPath == Path.DirectorySeparatorChar + ".")
+        {
+            return new DirectoryInfo(currentPath);
+        }
+        
+        DirectoryInfo currentDirectory = new DirectoryInfo(currentPath);
+        ManagePathDots(ref requestedPath, ref currentDirectory);
+
+        string pathAfterDots = currentDirectory.FullName;
+        if (requestedPath != string.Empty)
+        {
+            if (requestedPath.StartsWith(currentPath))
+            {
+                requestedPath = requestedPath[currentPath.Length..];
+            }
+            pathAfterDots += requestedPath;
+        }
+
+        return new DirectoryInfo(pathAfterDots);
+    }
+
+    /// <summary>
+    /// Changes the given directory if there are .. commands in path.
+    /// Removes managed .. command from the path.
+    /// </summary>
+    private void ManagePathDots(ref string path, ref DirectoryInfo currentDirectory)
+    {
+        var command = Path.DirectorySeparatorChar + "..";
+        while (path.StartsWith(command))
+        {
+            currentDirectory = currentDirectory.Parent ?? currentDirectory;
+            // Remove the managed command from the path.
+            path = path[command.Length..];
+        }
+    }
+
+    public string GetHelpMessage() => "cd {path}" + 
+                                      "\n The path can be absolute or relative.";
+    
+    public ConsoleState Process(string inpCommand, DirectoryInfo currentDir)
+    {
+        // Getting the path the user has given.
+        string requestedPath;
+        try
+        {
+            requestedPath = GetRequestedPath(inpCommand);
+        }
+        catch (CdCommnandEmptyPathException)
+        {
+            return NoMoveDueError("No path was given.", currentDir);
+        }
+        
+        
+        // The directory to work with while managing user's commands.
+        DirectoryInfo directoryUnderChange = ManagePath(requestedPath, currentDir.FullName);
+        
+        
+        
+        // Checking if the result directory is still in the root directory.
+        if (IsInRoot(directoryUnderChange) is false)
+        {
+            return NoMoveDueError(
+                $"Cannot leave the root directory: {ConsoleEngine.RootDir.FullName}",
+                currentDir);
+        }
+        
+        // Checking if the result directory exists.
+        if (Directory.Exists(directoryUnderChange.FullName) is false)
+        {
+            return NoMoveDueError(
+                "The given directory does not exist",
+                currentDir);
+        }
+
+        // If everything is OK, return new DirectoryInfo.
+        return new ConsoleState
+        {
+            CurrentDir = directoryUnderChange,
+            Result = ""
+        };
+    }
+}
+
+/// <summary>
+/// Occurs when user types cd command without giving any path.
+/// </summary>
+public class CdCommnandEmptyPathException : ApplicationException
+{
+    public CdCommnandEmptyPathException() : base("No path was given."){}
+    public CdCommnandEmptyPathException(string message) : base(message) {}
+    public CdCommnandEmptyPathException(string message, Exception inner) : base(message, inner){}
 }
